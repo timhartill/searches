9a36ee635cba6c51f446b7c2e13022f578164c66
timhartill <timhartill@gmail.com>
2025-09-29 17:49:21 +1300

    run_test_std_inconsist for bae* no ceil inconsistent

--- code/run_test_std_inconsistent_h.sh
+++ code/run_test_std_inconsistent_h.sh
@@ -277,7 +277,7 @@ comment
 
 python search_runner.py \
         --out_dir "../outputs" \
-        --out_prefix "lb_StdAllInconsistent_setid46and50_hgx2_clbmono_hog2opt_" \
+        --out_prefix "lb_StdAllInconsistent_setid47baenoceil_hgx2_clbmono_hog2opt_" \
         --in_dir "../problems" \
         --seed 42 \
         --rust_heur \
@@ -304,7 +304,7 @@ python search_runner.py \
         --algo_visualise \
         --algo_timeout 2880 \
         --algo_min_remaining_gb 2.0 \
-        --algo_heur lb_mwvcsmallg_lowg_none_dvcbs_a_eps_uf lb_mwvcsmallg_lowg_none_dvcbs_f_eps_uf lb_mwvcsmallgf_lowg_none_dvcbs_f_eps_uf lb_gbfhs_gbfhs_f_eps_uf lb_gbfhs_lowg_f_eps_uf lb_mostconnectednodes_lowg_f_eps_uf lb_smallg_lowg_none_f_eps_uf lb_smallgf_smallblowg_f_eps_uf lb_mwvcsmallg_lowg_none_dvcbs_a_eps_uf_bpmx1 lb_mwvcsmallg_lowg_none_dvcbs_f_eps_uf_bpmx1 lb_mwvcsmallgf_lowg_none_dvcbs_f_eps_uf_bpmx1 lb_gbfhs_gbfhs_f_eps_uf_bpmx1 lb_gbfhs_lowg_f_eps_uf_bpmx1 lb_mostconnectednodes_lowg_f_eps_uf_bpmx1 lb_smallg_lowg_none_f_eps_uf_bpmx1 lb_smallgf_smallblowg_f_eps_uf_bpmx1 \
+        --algo_heur lb_alter_first_none_bae_a_eps_ug lb_pohl_first_none_bae_a_eps_ug lb_minb_first_none_bae_a_eps_ug lb_rand_first_none_bae_a_eps_ug lb_alter_first_none_bae_a_eps_ug_bpmx1 lb_pohl_first_none_bae_a_eps_ug_bpmx1 lb_minb_first_none_bae_a_eps_ug_bpmx1 lb_rand_first_none_bae_a_eps_ug_bpmx1 \
         --algo_mcts NONE
 
 
