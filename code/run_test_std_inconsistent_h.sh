9ab1123d574fd49a0c29b8b18bb43edfaa6e9479
timhartill <timhartill@gmail.com>
2025-11-04 09:27:47 +1300

    add set 53

--- code/run_test_std_inconsistent_h.sh
+++ code/run_test_std_inconsistent_h.sh
@@ -285,7 +285,7 @@ comment
 
 python search_runner.py \
         --out_dir "../outputs" \
-        --out_prefix "lb_StdAllInconsistent_setid52bae_ug__bpmx_ceilgcd_hgx2_clbmono_hog2opt_" \
+        --out_prefix "lb_StdAllInconsistent_setid53bae_ug__bpmx_ceilgcd_hgx2_clbmono_hog2opt_" \
         --in_dir "../problems" \
         --seed 42 \
         --rust_heur \
@@ -312,7 +312,7 @@ python search_runner.py \
         --algo_visualise \
         --algo_timeout 2880 \
         --algo_min_remaining_gb 2.0 \
-        --algo_heur lb_mwvcsmallg_lowg_none_dvcbs_a_eps_uf_bpmx1 lb_mwvcsmallg_lowg_none_dvcbs_f_eps_uf_bpmx1 \
+        --algo_heur lb_mwvcsmallgf_lowg_none_dvcbs_f_eps_uf_bpmx1 lb_mostconnectednodes_lowg_f_eps_uf_bpmx1 \
         --algo_mcts NONE
 
 
